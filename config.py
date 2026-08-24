3d433c6a3619637712fb38d894048ffad6f5856d
foch-ka <fockaaaaaa@gmail.com>
2026-08-24 21:50:17 +0700

    Update config.py

--- config.py
+++ config.py
@@ -24,6 +24,13 @@ if TOPIC_ID:
 else:
     TOPIC_ID = None
 
+# Список разрешённых пользователей для ЛС (через запятую)
+ALLOWED_USERS = os.getenv("ALLOWED_USERS", "")
+if ALLOWED_USERS:
+    ALLOWED_USERS = [int(x.strip()) for x in ALLOWED_USERS.split(",") if x.strip().isdigit()]
+else:
+    ALLOWED_USERS = []
+
 FORUM_URLS = {
     "complaint_staff": "https://forum.vimeworld.com/forum/195-%D0%B6%D0%B0%D0%BB%D0%BE%D0%B1%D1%8B-%D0%BD%D0%B0-%D0%BF%D0%B5%D1%80%D1%81%D0%BE%D0%BD%D0%B0%D0%BB/",
     "complaint_player": "https://forum.vimeworld.com/forum/189-%D0%B6%D0%B0%D0%BB%D0%BE%D0%B1%D1%8B-%D0%BD%D0%B0-%D0%B8%D0%B3%D1%80%D0%BE%D0%BA%D0%BE%D0%B2/",
